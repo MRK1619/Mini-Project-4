2276adc56f4f81356a3d7f698e35c8e6cf2fa11a
Partha Sarathi Hazra <sarathiparthahazra9609@gmail.com>
2022-06-02 09:37:35 +0530

    Spelling Correction

---
+++ project27.py
@@ -0,0 +1,10 @@
+# Spelling Correction
+from textblob import TextBlob
+words = ["engennring", "technolo"]
+corrected_words = []
+for i in words:
+    corrected_words.append(TextBlob(i))
+print("Wrong words :", words)
+print("Corrected Words are :")
+for i in corrected_words:
+    print(i.correct(), end=" ")
