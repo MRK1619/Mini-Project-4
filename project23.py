7c211d700b9209cdea807f9a26097c4d68dcdd15
Partha Sarathi Hazra <sarathiparthahazra9609@gmail.com>
2022-06-02 08:14:41 +0530

    Print Colour Text

---
+++ project23.py
@@ -0,0 +1,8 @@
+#Print Colour Text
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Partha Sarathi Hazra "+ Fore.YELLOW+ Back.BLUE+"Abhik Mondal")
+print(Back.CYAN+"From Digha")
+print(Fore.RED + Back.GREEN+ "Near Deulihat")
